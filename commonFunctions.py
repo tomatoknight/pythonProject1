7b7b1083f032b66eb87f9692881fa850d76b4a9b
tomatoknight <jitana06@gmail.com>
2022-03-01 15:41:37 +0800

    test normal change of Numpy model

--- commonFunctions.py
+++ commonFunctions.py
@@ -12,11 +12,20 @@ import numpy as np
 # e = np.arange(4).reshape(1, 4)
 # print(np.broadcast_to(e, (4, 4)))
 
-a = np.arange(9).reshape(3, 1, 3)
-print(a)
-print(".................")
-b = np.arange(9).reshape(1, 3, 3)
-print(b)
-print(".................")
-c = np.arange(9).reshape(3, 3, 1)
-print(c)
+# a = np.arange(9).reshape(3, 1, 3)
+# print(a)
+# print(".................")
+# b = np.arange(9).reshape(1, 3, 3)
+# print(b)
+# print(".................")
+# c = np.arange(9).reshape(3, 3, 1)
+# print(c)
+#
+# print(a.shape, b.shape, c.shape)
+
+d = np.arange(6)
+d1 = np.split(d, 2)
+print(d1)
+
+d2 = np.split(d, [2, 3])
+print(d2)
